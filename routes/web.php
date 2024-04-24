c662dc8b92578ef36e3a15ec8cc8d2e7ec88c4c0
TruongVicent <110811587+TruongVicent@users.noreply.github.com>
2024-04-24 18:59:35 +0700

    [Client] Controller của giao diện

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Client\Home;
+use App\Http\Controllers\Client\Product;
+use App\Http\Controllers\Client\DetailHome;
+use App\Http\Controllers\Client\Checkout;
+use App\Http\Controllers\Client\CartHome;
+use App\Http\Controllers\Client\Body;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('/')->group(function (){
+    Route::get('/',[Home::class,'index']);
+    Route::get('/',[Body::class,'index']);
+    route::get('/product',[Product::class,'index']);
+    Route::get('/detail',[DetailHome::class,'index']);
+    Route::get('/checkout',[Checkout::class,'index']);
+    Route::get('/cart',[CartHome::class,'index']);
 });
