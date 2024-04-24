27655eb186e0f6619b3e080259179d295679bd5e
TruongVicent <110811587+TruongVicent@users.noreply.github.com>
2024-04-24 19:03:16 +0700

    [Client] Các css,js,image và cdn của giao diện

---
+++ public/assets/js/script.js
@@ -0,0 +1,84 @@
+// ---------------
+// count down
+// ---------------
+let dateupto = new Date("Sep 30, 2024 23:59:59:999");
+
+if (dateupto < new Date()) {
+    $("#countdown").hide();
+    $("#countdownbtn").hide();
+    $("#normalbtn").show();
+} else {
+    $("#countdown").show();
+    $("#normalbtn").hide();
+}
+
+
+let $days = document.getElementById('days');
+let $hours = document.getElementById('hours');
+let $minutes = document.getElementById('minutes');
+let $seconds = document.getElementById('seconds');
+
+setInterval(function () {
+    // Calculate the remaining time
+    var now = new Date();
+    var timeLeft = (dateupto - now) / 1000;
+    updateClock(timeLeft);
+}, 1000);
+
+function updateClock(remainingTime) {
+    // calculate (and subtract) whole days
+    let days = Math.floor(remainingTime / 86400);
+    remainingTime -= days * 86400;
+
+    // calculate (and subtract) whole hours
+    let hours = Math.floor(remainingTime / 3600) % 24;
+    remainingTime -= hours * 3600;
+
+    // calculate (and subtract) whole minutes
+    let minutes = Math.floor(remainingTime / 60) % 60;
+    remainingTime -= minutes * 60;
+
+    // what's left is seconds
+    let seconds = Math.floor(remainingTime % 60);
+
+    // pad numbers if needed
+    $days.innerHTML = padNumber(days);
+    $hours.innerHTML = padNumber(hours);
+    $minutes.innerHTML = padNumber(minutes);
+    $seconds.innerHTML = padNumber(seconds);
+}
+
+function padNumber(number) {
+    return number < 10 ? "0" + number : number;
+}
+
+
+// ---------------
+// change image
+// ---------------
+function changeImg(pic){
+    document.getElementById('change_image').src = pic;
+}
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/assets/js/slick.js
@@ -0,0 +1,134 @@
+$('.autoplay').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+ 
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4,
+            infinite: false
+        }
+
+    },
+    {
+
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 2,
+            dots: false
+
+        }
+
+    },
+    {
+
+        breakpoint: 540,
+        settings: {
+            slidesToShow: 2,
+            dots: false
+        }
+
+    }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+    }]
+});
+
+
+
+
+
+
+
+$('.autoplay-country').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    infinite: true,
+    rows: 2,
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4,
+            infinite: false
+        }
+
+    },
+    {
+
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 3,
+            dots: false
+
+        }
+
+    },
+    {
+
+        breakpoint: 540,
+        settings: {
+            slidesToShow: 2,
+            dots: false
+        }
+
+    }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+    }]
+});
+
+
+
+
+$('.autoplay-block').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    infinite: true,
+    rows: 2,
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 769,
+        settings: {
+            slidesToShow: 3,
+            dots: false
+        }
+
+    },
+   
+    {
+
+        breakpoint: 540,
+        settings: {
+            slidesToShow: 3,
+            dots: false,
+            rows: 1,
+        }
+
+    }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+    }]
+});
