d56e07468be8b8e3d7e094cf1ed095aa821a5fa6
Minh-Truong <nguyenmingtruong9@gmail.com>
2024-06-20 11:07:54 +0700

    Enum trạng thái order

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -9,56 +9,43 @@ use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasLabel;
 enum OrderStatus: string implements HasLabel, HasIcon, HasColor
 {
-    case News = 'Mới';
-
+    case New = 'Mới';
     case Processing = 'Đang xử lý';
-
     case Shipped = 'Đã vận chuyển';
-
     case Delivered = 'Đã giao hàng';
-
     case Cancelled = 'Đã hủy bỏ';
-
-    public function getName(): string
-    {
+    const OnHold =  'Đơn tạm giữ';
+    public function getLabel(): string {
         return match ($this) {
-            self::News => 'Mới',
+            self::New => 'Mới',
             self::Processing => 'Đang xử lý',
-            self::Shipped =>'Đã vận chuyển',
+            self::Shipped => 'Đã vận chuyển',
             self::Delivered => 'Đã giao hàng',
             self::Cancelled => 'Đã hủy bỏ',
+            self::OnHold => 'Đơn tạm giữ',
         };
     }
 
-    public function getLabel(): string
-    {
+    public function getColor(): string|array|null {
         return match ($this) {
-            self::News => 'Mới',
-            self::Processing => 'Đang xử lý',
-            self::Shipped =>'Đã vận chuyển',
-            self::Delivered => 'Đã giao hàng',
-            self::Cancelled => 'Đã hủy bỏ',
-        };
-    }
-
-    public function getColor(): string|array|null
-    {
-        return match ($this) {
-            self::News => 'info',
+            self::New => 'info',
             self::Processing => 'warning',
             self::Shipped, self::Delivered => 'success',
             self::Cancelled => 'danger',
+            self::OnHold  => 'gray',
         };
     }
 
-    public function getIcon(): ?string
-    {
+    public function getIcon(): ?string {
         return match ($this) {
-            self::News => 'heroicon-m-sparkles',
+            self::New => 'heroicon-m-sparkles',
             self::Processing => 'heroicon-m-arrow-path',
             self::Shipped => 'heroicon-m-truck',
             self::Delivered => 'heroicon-m-check-badge',
             self::Cancelled => 'heroicon-m-x-circle',
+            self::OnHold  => 'heroicon-m-finger-print
+
+',
         };
     }
 }
